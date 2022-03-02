8c6ab5659aae33d61c6f14ab5d76c8216f0591f2
Paulo Hericles Neves da Silva <t02624@complexo.unimed>
2022-03-02 08:31:09 -0300

    add Subject

--- src/app/shared/lesson-selected.service.ts
+++ src/app/shared/lesson-selected.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, filter } from 'rxjs';
+import { BehaviorSubject, filter, Subject } from 'rxjs';
 import { Lesson } from '.';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LessonSelectedService {
-  private _selected = new BehaviorSubject<Lesson | null>(null);
+  private _selected = new Subject<Lesson>();
 
   public selected$ = this._selected.asObservable().pipe(filter(lesson => !!lesson));
 
